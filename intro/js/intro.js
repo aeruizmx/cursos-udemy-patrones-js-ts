e526a28b5e899ba97eb98446683b90959dcb037d
aruiz <aruiz@grupooctano.com.mx>
2022-12-08 11:58:25 -0600

    Clase 3

--- intro/js/intro.js
+++ intro/js/intro.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo')
+function sum(a,b)
+{
+  return a+b;
+}
+let res = sum(1,2);
+console.log(res);
+// Funciones de primer orden, guardadas en variables
+const fSum = sum;
+res = fSum(5,5);
+console.log(res);
+
+function operation (fn, a, b){
+  console.log('Se hace algo');
+  console.log(fn(a,b))
+}
+// Funciones de orden superior, que permiten recibir funciones
+
+operation(sum, 4,5);
