f387243e5e852fe6e3926c1bc25cab9dfdc4f212
aruiz <aruiz@grupooctano.com.mx>
2022-12-15 13:40:44 -0600

    Clase 12 Interfaces en TS

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,6 +1,6 @@
 console.log("Hola desde typescript");
 
-class Drink {
+class Bebida {
   private name: string;
   constructor(name: string){
     this.name = name;
@@ -9,19 +9,57 @@ class Drink {
     return this.name;
   }
 }
-const drink = new Drink("agua");
+const drink = new Bebida("agua");
 console.log(drink.info());
 
-class Beer extends Drink{
-  private alcohol: number;
+// Interface
+interface Producto{
+  price: number;
+  getPrice(): string;
+}
 
-  constructor(name: string, alcohol: number){
+class Cerveza extends Bebida implements Producto{
+  private alcohol: number;
+  price: number;
+  
+  constructor(name: string, alcohol: number, price: number){
     super(name);
     this.alcohol = alcohol;
+    this.price = price;
+  }
+  getPrice(): string{
+    return "$ "+this.price;
   }
   info(): string{
     return super.info() + " " + this.alcohol;
   }
 }
-const beer = new Beer('corona',4.5);
+// Implementacion de interface
+class Snack implements Producto{
+  name: string;
+  price: number;
+  constructor(name: string, price: number){
+    this.name = name;
+    this.price = price;
+  }
+  getPrice(): string{
+    return "El precio es: $ " + this.price;
+  }
+}
+const beer = new Cerveza("erdinger", 8.5, 100);
 console.log(beer.info());
+
+const products: Producto[] = [
+  new Cerveza("Corona", 4.5, 1),
+  new Snack("Sabritas", 0.5),
+  new Cerveza("Heineken", 5, 1.2),
+]
+
+function getPrices(items: Producto[]){
+  for (const item of items) {
+    console.log(item.getPrice());
+    
+  }
+}
+
+getPrices(products);
