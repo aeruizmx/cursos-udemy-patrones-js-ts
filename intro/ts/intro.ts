69e6da3a324c37a5b8b8f0b53ffc66a82527be61
aruiz <aruiz@grupooctano.com.mx>
2022-12-15 13:05:41 -0600

    Clase 10 y 11 Clases, Objectos, Herencia en TS

--- intro/ts/intro.ts
+++ intro/ts/intro.ts
@@ -1,1 +1,27 @@
 console.log("Hola desde typescript");
+
+class Drink {
+  private name: string;
+  constructor(name: string){
+    this.name = name;
+  }
+  info(): string{
+    return this.name;
+  }
+}
+const drink = new Drink("agua");
+console.log(drink.info());
+
+class Beer extends Drink{
+  private alcohol: number;
+
+  constructor(name: string, alcohol: number){
+    super(name);
+    this.alcohol = alcohol;
+  }
+  info(): string{
+    return super.info() + " " + this.alcohol;
+  }
+}
+const beer = new Beer('corona',4.5);
+console.log(beer.info());
